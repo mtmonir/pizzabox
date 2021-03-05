699618f502d4250c19d869b7388bf63bbac3a7a6
tahir <tahir.monir@gmail.com>
2021-03-05 13:46:03 -0600

    initial commit

---
+++ PizzaBox.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using PizzaBox.Domain.Abstracts;
+using PizzaBox.Domain.Models;
+using PizzaBox.Domain.Singletons;
+
+namespace PizzaBox.Client
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      PlayWithStore();
+    }
+
+    public static void PlayWithStore()
+    {
+      var storeSingleton = new StoreSingleton();
+
+      foreach (var store in storeSingleton.Stores)
+      {
+        Console.WriteLine(store);
+      }
+    }
+  }
+}
---
+++ PizzaBox.Domain/Models/FreddyPizza.cs
@@ -0,0 +1,15 @@
+
+
+using PizzaBox.Domain.Abstracts;
+
+namespace PizzaBox.Domain.Models
+{
+  public class FreddyPizza : AStore
+  {
+      public FreddyPizza()
+      {
+          Name = "Freedy Store";
+      }
+  
+  }
+}
